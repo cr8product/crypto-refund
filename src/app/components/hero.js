f0ccf8a95a940c8a9b5244fa9a87e6ab54016447
Begum SA <begumsener94@gmail.com>
2023-07-21 18:19:00 +0000

    daisyuı-update

--- src/app/components/hero.js
+++ src/app/components/hero.js
@@ -1,5 +1,4 @@
-'use client'
-import currentBlockNo  from "./web3/utils"
+
 
 export default function Hero() {
   return (
@@ -9,7 +8,7 @@ export default function Hero() {
         <img src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg" className="max-w-sm rounded-lg shadow-2xl" />
         <div>
           <h1 className="text-5xl font-bold">Discover the world with us!</h1>
-          <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. <br />Current Block No.: <b>{currentBlockNo()}</b> from QuickNode</p>
+          <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. </p>
           <button className="btn btn-primary">Connect Wallet</button>
         </div>
       </div>
