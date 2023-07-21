b43c1106478155c4c094e0aea26cc6391b05a79e
Begum SA <begumsener94@gmail.com>
2023-07-21 19:36:43 +0000

    hero-paragraph-update

--- src/app/components/hero.js
+++ src/app/components/hero.js
@@ -7,8 +7,8 @@ export default function Hero() {
       <div className="hero-content flex-col lg:flex-row-reverse">
         <img src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg" className="max-w-sm rounded-lg shadow-2xl" />
         <div>
-          <h1 className="text-5xl font-bold">Discover the world with us!</h1>
-          <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. </p>
+          <h1 className="text-5xl font-bold">Shop with crypto refund whereever you want!</h1>
+          <p className="py-6">No need to share your identity details with others. Decentralized approach to taxfree shopping. </p>
           <button className="btn btn-primary">Connect Wallet</button>
         </div>
       </div>
