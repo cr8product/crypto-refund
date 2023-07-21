ffbaa94c6a189a3fcbc2e4cbbef62122ad35a817
Begum SA <begumsener94@gmail.com>
2023-07-21 17:45:52 +0000

    navbar-added

---
+++ src/app/components/navbar.js
@@ -0,0 +1,16 @@
+
+
+export default function Navbar() {
+    return (
+        <div className="navbar bg-base-100">
+            <div className="flex-1">
+                <a className="btn btn-ghost normal-case text-xl">Cr8Product</a>
+            </div>
+            <div className="flex-none">
+                <ul className="menu menu-horizontal px-1">
+                    <button className="btn btn-active btn-primary">Connect Wallet</button>
+                </ul>
+            </div>
+        </div>
+    )
+}
