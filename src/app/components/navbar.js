38073fbce1fbf4f46119286270f251d1af936d3c
Begum SA <begumsener94@gmail.com>
2023-07-21 19:36:54 +0000

    navbar-upd

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
     return (
         <div className="navbar bg-base-100">
             <div className="flex-1">
-                <a className="btn btn-ghost normal-case text-xl">Cr8Product</a>
+                <a className="btn btn-ghost normal-case text-xl">CryptoRefund</a>
             </div>
             <div className="flex-none">
                 <ul className="menu menu-horizontal px-1">
