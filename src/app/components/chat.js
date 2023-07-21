092b4a6936886a44e37a9be99700ec94bf0f7350
Begum SA <begumsener94@gmail.com>
2023-07-21 20:34:35 +0000

    chat-component-added

--- src/app/components/chat.js
+++ src/app/components/chat.js
@@ -8,14 +8,14 @@ export default function Chat() {
       <div className="chat chat-start">
         <div className="chat-image avatar">
           <div className="w-10 rounded-full">
-            <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
           </div>
         </div>
         <div className="chat-header">
           Obi-Wan Kenobi
           <time className="text-xs opacity-50">12:45</time>
         </div>
-        <div className="chat-bubble">You were the Chosen One!</div>
+        <div className="chat-bubble">Hello support!</div>
         <div className="chat-footer opacity-50">
           Delivered
         </div>
@@ -30,7 +30,7 @@ export default function Chat() {
           Anakin
           <time className="text-xs opacity-50">12:46</time>
         </div>
-        <div className="chat-bubble">I hate you!</div>
+        <div className="chat-bubble">Hello! How can I help you?</div>
         <div className="chat-footer opacity-50">
           Seen at 12:46
         </div>
