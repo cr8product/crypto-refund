0dd353ba8bcfe4cf84f4ed2e4757dffdeea91988
Begum SA <begumsener94@gmail.com>
2023-07-21 18:11:09 +0000

    card-update

--- src/app/page.js
+++ src/app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
       <h1 className="text-4xl text-center p-10 font-extrabold">Discover us in Three Easy Steps</h1>
       <div className="flex justify-center justify-evenly">
       
-      <Card title="Go Shopping" image="https://cdn.pixabay.com/photo/2017/12/26/09/15/woman-3040029_1280.jpg" description=" Go Shopping" button="More"/>
+      <Card title="Go Shopping" image="https://www.flaticon.com/free-icon/online-shopping_3081559?term=shopping&page=1&position=4&origin=search&related_id=3081559" description=" Go Shopping" button="More"/>
       <Card title="About us" image="https://www.flaticon.com/free-icon/custom_2760975?term=customs&page=1&position=17&origin=search&related_id=2760975" description=" Customs Validation" button="Next"/>
       <Card title="Refund" image="https://www.flaticon.com/free-icon/refund_1642307?term=refund&page=1&position=30&origin=search&related_id=1642307" description=" Get Refund Back" button="Skip"/>
       </div>
