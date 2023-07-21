b3d061bf148f030e5252774d1fbef2f66e80b5c5
Begum SA <begumsener94@gmail.com>
2023-07-21 19:41:34 +0000

    page-update

--- src/app/page.js
+++ src/app/page.js
@@ -11,9 +11,9 @@ export default function Home() {
       <h1 className="text-4xl text-center p-10 font-extrabold">Discover us in Three Easy Steps</h1>
       <div className="flex justify-center justify-evenly">
       
-      <Card title="Go Shopping" image="https://www.flaticon.com/free-icon/online-shopping_3081559?term=shopping&page=1&position=4&origin=search&related_id=3081559" description=" Go Shopping" button="More"/>
-      <Card title="About us" image="https://www.flaticon.com/free-icon/custom_2760975?term=customs&page=1&position=17&origin=search&related_id=2760975" description=" Customs Validation" button="Next"/>
-      <Card title="Refund" image="https://www.flaticon.com/free-icon/refund_1642307?term=refund&page=1&position=30&origin=search&related_id=1642307" description=" Get Refund Back" button="Skip"/>
+      <Card title="Go Shopping" image="https://img.freepik.com/free-vector/seasonal-sale-discounts-presents-purchase-visiting-boutiques-luxury-shopping-price-reduction-promotional-coupons-special-holiday-offers-vector-isolated-concept-metaphor-illustration_335657-2766.jpg?w=900&t=st=1689968298~exp=1689968898~hmac=2c5e496442940c140eddcd78ab10106dcafd2c67b6c0cefd406e282b67c5d3ef" description=" Track your tax free process by connecting your wallet." button="More"/>
+      <Card title="About us" image="https://img.freepik.com/free-vector/customs-clearance-abstract-concept-illustration_335657-3935.jpg?w=900&t=st=1689968363~exp=1689968963~hmac=1e7b4d19426e589147099569ef41021e0612818141ce8b6430b83298a21d24d9" description=" Get confirmation from customs officers." button="Next"/>
+      <Card title="Refund" image="https://img.freepik.com/free-vector/e-shopping-cartoon-web-icon-online-store-cashback-service-money-returning-financial-refund-idea-return-investment-internet-income-vector-isolated-concept-metaphor-illustration_335657-2734.jpg?w=900&t=st=1689968387~exp=1689968987~hmac=51daaf3112ace361760a6674dcb9894a779c5252335a0cf0db5f5122a6b61f36" description=" Refunds will be back via your wallet." button="Skip"/>
       </div>
       <h1 className="text-4xl text-center p-10 font-extrabold"></h1>
       <Footer/>
