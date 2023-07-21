64a7d8661e1e5dd1ff9a88b06d5aa39ca7896f7c
Begum SA <begumsener94@gmail.com>
2023-07-21 17:49:30 +0000

    card-component-added

---
+++ src/app/components/card.js
@@ -0,0 +1,15 @@
+
+export default function Card(props){
+    return(
+    <div className="card w-96 glass">
+        <figure><img src={props.image} alt="car!" /></figure>
+        <div className="card-body">
+            <h2 className="card-title">{props.title}</h2>
+            <p>{props.description}</p>
+            <div className="card-actions justify-end">
+                <button className="btn btn-primary">{props.button}</button>
+            </div>
+        </div>
+    </div>
+    )
+    }
