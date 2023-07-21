10240734fcceccee8ef5213b7e7cc4cbade4b1a7
Begum SA <begumsener94@gmail.com>
2023-07-21 22:44:50 +0000

    updates

--- src/app/components/hero.js
+++ src/app/components/hero.js
@@ -3,15 +3,15 @@
 export default function Hero() {
   return (
 
-    <div className="hero min-h-screen bg-base-200">
+    <div className="hero min-h-screen bg-opacity-60"  style={{backgroundImage: 'url(https://img.freepik.com/free-vector/dark-green-abstract-background_1100-129.jpg?w=900&t=st=1689979415~exp=1689980015~hmac=4d3d2c5641ba1cf11d0ec811276d6635ed0204bdd4e7d41d4820c37799767730)'}}>
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <img src="https://img.freepik.com/free-vector/buying-tickets-online-abstract-concept-illustration_335657-2229.jpg?w=900&t=st=1689968568~exp=1689969168~hmac=ead463284c6aa42cb8c7bf2c123a20f7756dc45ba6b7dfbbf3b526cf287a3dac" className="max-w-sm rounded-lg shadow-2xl" />
         <div>
           <h1 className="text-5xl font-bold">Shop with crypto refund whereever you want!</h1>
           <p className="py-6">No need to share your identity details with others. Decentralized approach to taxfree shopping. </p>
-          <button className="btn btn-primary">Connect Wallet</button>
+          <button className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Connect Wallet</button>
         </div>
       </div>
     </div>
+    
   )
 }
--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -11,7 +11,7 @@ export default function Navbar() {
             </div>
             <div className="flex-none">
                 <ul className="menu menu-horizontal px-1">
-                    <a className="btn btn-blue">Contact us</a>
+                    <a className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Contact us</a>
                     <Web3Button>Connect Wallet</Web3Button>
                 </ul>
             </div>
