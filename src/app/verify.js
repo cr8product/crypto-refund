cabf6663fcf54b92a19aef710acfd24682861a5e
Begum SA <begumsener94@gmail.com>
2023-07-21 19:52:56 +0000

    verify-page-set-up

---
+++ src/app/verify.js
@@ -0,0 +1,17 @@
+import Card from "./components/card";
+import Navbar from "./components/navbar";
+import Steps from "./components/steps";
+
+
+export default function Verfiy() {
+    return (
+        <div>
+
+            <Navbar />
+            <Card />
+            <Steps/>
+            
+
+        </div>
+    )
+}
