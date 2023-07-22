3256a0e11dde4677d899d36fec77a07524c3b18c
Metin A <test@metoinside.com>
2023-07-22 19:28:36 +0000

    env crashed

--- src/app/sc/page.js
+++ src/app/sc/page.js
@@ -4,11 +4,14 @@ import contractArtifact from "../../../artifacts/contracts/Greeter.sol/Greeter.j
 import React from "react";
 
 export default function smartContractInteractions() {
+    const INFURA_KEY = "45cb05f851444a51bfd995aef63494d9";
+    const PRIVATE_KEY = "9e7205b9cb030435e225ada16e30144e7ddc638da10f063e83230b97d4191eda";
+
     const deployedContract = "0x085E82226a3709105883aA0fa2680279Db13B46f";
-    const provider = new ethers.JsonRpcProvider(`https://goerli.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}`)
+    const provider = new ethers.JsonRpcProvider(`https://goerli.infura.io/v3/${INFURA_KEY}`)
     
     const readContract = new ethers.Contract(deployedContract, contractArtifact.abi, provider);
-    const signer = new ethers.Wallet(process.env.NEXT_PUBLIC_PRIVATE_KEY, provider);
+    const signer = new ethers.Wallet(PRIVATE_KEY, provider);
     const writeContract = new ethers.Contract(deployedContract, contractArtifact.abi, signer);
 
     const [value, setValue] = React.useState("empty")
@@ -19,7 +22,7 @@ export default function smartContractInteractions() {
     }
         
     async function submitTx() {
-        const tx = await writeContract.setGreeting("This is 6");
+        const tx = await writeContract.setGreeting("This is 9");
         alert("Transaction submitted: ", tx.hash);
     }
         
