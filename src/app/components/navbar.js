9b75e45d88ad39d1c7f8ff82a8aca3c847884116
Begum SA <begumsener94@gmail.com>
2023-07-22 11:40:00 +0000

    navigate-on-same-page

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -1,17 +1,18 @@
 "use client"
 
 import {Web3Button} from "@web3modal/react";
-
+import Link from 'next/link'
 
 export default function Navbar() {
     return (
         <div className="navbar bg-base-100">
             <div className="flex-1">
+
                 <a className="btn btn-ghost normal-case text-xl">CryptoRefund</a>
             </div>
             <div className="flex-none">
                 <ul className="menu menu-horizontal px-1">
-                    <a className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Contact us</a>
+                    <Link href="https://3001-cr8product-cryptorefund-96fdd96acmp.ws-eu102.gitpod.io/support" className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Contact us</Link>
                     <Web3Button>Connect Wallet</Web3Button>
                 </ul>
             </div>
