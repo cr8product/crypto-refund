b4b699ee55e87a45d1395c50c8009b9de620e79f
Begum SA <begumsener94@gmail.com>
2023-07-22 08:06:20 +0000

    customs-update

--- src/app/customs/page.js
+++ src/app/customs/page.js
@@ -1,3 +1,4 @@
+import Footer from "../components/footer";
 import Navbar2 from "../components/navbar2";
 import Textbox from "../components/textbox";
 
@@ -6,15 +7,20 @@ export default function Customs() {
     return (
         <div>
             <Navbar2 title="Customs" />
-
+            <div className="grid h-10 rounded-box place-items-center"></div>
             <div className="flex justify-center justify-evenly">
                 <Textbox name="Receipt Document ID" description="I.E: 1234567890" />
                 <Textbox name="Value" description="I.E: $USDC" />
-                </div>
-                <div className="grid h-20 card bg-base-300 rounded-box place-items-center">Scan Tax Free form</div>
-                <button className="btn btn-warning btn-wide ">Approve</button>
-                
             </div>
+            <div className="grid h-10 rounded-box place-items-center"></div>
+            
+            <div className="flex justify-center justify-evenly">
+            <button className="btn btn btn-info btn-wide ">Approve</button>
+            <button className="btn btn-warning btn-wide ">Reject</button>
+        </div>
+        <div className="grid h-20 rounded-box place-items-center"></div>
+        <Footer/>
+        </div>
             
             
             )
